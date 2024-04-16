3bc5fb8cbaef0ea2733dddc361dc3205a8109f61
harry-tsai <harrytsai@17.media>
2024-04-16 13:51:11 +0800

    [WAVE-5295] Skip to verify specific collections

--- infra/mongodb-migration-verifier/main.py
+++ infra/mongodb-migration-verifier/main.py
@@ -10,6 +10,8 @@ MONGODB_TARGET_URI = os.getenv("MONGODB_TARGET_URI")
 
 DATABASE_NAME = "wave"
 
+SKIP_VERIFIED_COLLECTIONS = set(["test"])
+
 
 class ReportItemStatus(Enum):
     PASSED = 1
@@ -56,7 +58,7 @@ class Report(object):
             if i.status == ReportItemStatus.PASSED:
                 ret += f"{i.key}: {i.status.name}\n"
             else:
-                ret += f"{i.key}: {i.status.name} (source: {i.src_val} -- target: {i.target_val})\n"
+                ret += f"{i.key}: {i.status.name}\n  >>> source: {i.src_val}\n  >>> target: {i.target_val}\n"
 
         ret += f"TOTAL: {self.total}, PASSED: {self.passed}, FAILED: {self.failed}"
         return ret
@@ -64,7 +66,7 @@ class Report(object):
 
 class VerificationSet(object):
     def __init__(self) -> None:
-        self.items: Dict[str, int | str | List] = dict()
+        self.items: Dict[str, int | str | List | set] = dict()
 
     def add_item(self, key, value):
         if key in self.items:
@@ -122,23 +124,23 @@ def main():
 def get_verification_set_by_uri(mongo_uri: str, db_name: str) -> VerificationSet:
     cli = pymongo.MongoClient(mongo_uri)
     db = cli[db_name]
-    coll_names = db.list_collection_names()
-    set = VerificationSet()
-    set.add_item("collections_names", coll_names)
+    coll_names = set(db.list_collection_names()) - SKIP_VERIFIED_COLLECTIONS
+    vs = VerificationSet()
+    vs.add_item("collections_names", coll_names)
 
     for name in coll_names:
         pfx = f"collection_{name}"
         stats = db.command("collStats", name)
         count = stats['count']
-        set.add_item(f"{pfx}.doc_count", count)
-        set.add_item(f"{pfx}.indexes",
+        vs.add_item(f"{pfx}.doc_count", count)
+        vs.add_item(f"{pfx}.indexes",
                      get_indexes(stats['indexSizes']))
-        set.add_item(f"{pfx}.first_obj",
+        vs.add_item(f"{pfx}.first_obj",
                      find_one(db[name], pymongo.ASCENDING))
-        set.add_item(f"{pfx}.last_obj",
+        vs.add_item(f"{pfx}.last_obj",
                      find_one(db[name], pymongo.DESCENDING))
 
-    return set
+    return vs
 
 
 def get_indexes(indexInfos: Dict) -> List[str]:
